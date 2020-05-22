1952fc3d24c6578a11f0ec043146ef2abce565b3
Balibaloo <romankubiv101@gmail.com>
2020-05-22 11:37:05 +0000

    7

--- bot.js
+++ bot.js
@@ -1,17 +1,11 @@
 
 
 require('dotenv').config()
-
+const http = require("http")
+const fs = require('fs')
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-var StreamPlayer = require('stream-player');
-var player = new StreamPlayer();
-
-
-
-
 
 
 client.on('ready', () => {
@@ -19,22 +13,42 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+let playConn =(connection) => {
+    connection.play(fs.createReadStream(__dirname + "/TakeItBack.mp3"));
+    console.log("playing stream")
+    }
+
 
 client.on('message', message => {
 
     messageText = message.content
 
     if (messageText.startsWith("123")){
-        player.add('http://path-to-mp3.com/example.mp3', metaData);
 
-        // Start playing all songs added to the queue (FIFO)
-        player.play();
+        url = messageText.split(" ")[1]
+
+        if (message.member.voice.channel) {
+            message.member.voice.channel.join().then(playConn).catch(error => {
+            
+                message.member.voice.channel.join().then(playConn).catch(console.log)
+            });
+
+            
+
+            
+        } else {
+            message.channel.send('You need to join a voice channel first!');
+        }
+
+        
+
+        
+
     }
     
 
 });
 
 
-
 // login
-client.login(process.env.token);
+client.login(process.env.botToken);
